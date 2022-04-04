069b605a8d47f56bc12b1a0c3c764134b53e39cc
shivammandaliya <101386615+shivammandaliya@users.noreply.github.com>
2022-04-04 22:14:11 +0530

    Add files via upload

---
+++ Rainbow/sketch.js
@@ -0,0 +1,18 @@
+
+var colors = ["violet" , "indigo" , "blue" , "green" , "yellow" , "orange" , "red"];
+var currentColor;
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background("white");
+}
+
+
+function draw() {
+  var y =0; 
+  for (i = 0; i < colors.length; i++){
+     currentColor = new Band(0 , y , 400 , 60 , colors[i]);
+     currentColor.appear();
+     y = y + 50;
+ }
+}
